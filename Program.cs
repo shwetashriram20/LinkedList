06e820dc90c39b8b38dd93e8ee82078db0ca1e8a
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-11 20:04:21 +0530

    Main Method

--- Program.cs
+++ Program.cs
@@ -4,11 +4,22 @@
 {
     public static void Main(String[] args)
     {
+        LinkedList list = new LinkedList();
 
-        Console.WriteLine("Linked List: " );
 
-        
+            list.insertNode(56);
+            list.insertNode(30);
+            list.insertNode(70);
+            list.printNodes();
+
+
+            Console.WriteLine("Find 30 in the Lineked list");
+            list.findNode(30);
+            list.printNodes();
+
+        }
     }
-}       
 
 }
+          
+
