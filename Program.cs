81d2f45b38a374675d16551e81fb981cb5288a43
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-11 19:07:13 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,14 +4,10 @@
 {
     public static void Main(String[] args)
     {
-        LinkedList list = new LinkedList();
 
-        Console.WriteLine("Current Size of the list is: " + list.getListSize());
+        Console.WriteLine("Linked List: " );
 
-        list.insertNode(56);
-        list.insertNode(30);
-        list.insertNode(70);
-        list.printNodes();
+        
     }
 }       
 
