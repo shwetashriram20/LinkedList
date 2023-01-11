a4d35bebc7c430f0cb4754979be87681ebd4e08d
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-11 19:05:17 +0530

    Mainmethod

--- Program.cs
+++ Program.cs
@@ -1,18 +1,22 @@
 ﻿namespace LinkedList
 {
+    
     class Program
-{
-    public static void Main(String[] args)
     {
-        LinkedList list = new LinkedList();
+        public static void Main(String[] args)
+        {
+            LinkedList list = new LinkedList();
 
-        Console.WriteLine("Current Size of the list is: " + list.getListSize());
+            Console.WriteLine("Current Size of the list is: " + list.getListSize());
 
-        list.insertNode(56);
-        list.insertNode(30);
-        list.insertNode(70);
-        list.printNodes();
-    }
-}       
+            list.insertNode(56);
+            list.insertNode(30);
+            list.insertNode(70);
+            list.printNodes();
+        }
+    }       
 
 }
+    
+       
+
