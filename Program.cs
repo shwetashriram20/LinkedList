6befee12b9ebb62b9b11b5210c0e06044a5cfbd7
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-11 19:15:14 +0530

    Main Method

--- Program.cs
+++ Program.cs
@@ -4,10 +4,14 @@
 {
     public static void Main(String[] args)
     {
+        LinkedList list = new LinkedList();
 
-        Console.WriteLine("Linked List: " );
+        Console.WriteLine("Size of the list is: " + list.getListSize());
 
-        
+        list.insertNode(56);
+        list.insertNode(30);
+        list.insertNode(70);
+        list.printNodes();
     }
 }       
 
