6dfb64aea23cc71ab10d7b70ce99090a38e2c186
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-11 19:57:33 +0530

    method

--- Program.cs
+++ Program.cs
@@ -4,11 +4,22 @@
 {
     public static void Main(String[] args)
     {
+        LinkedList list = new LinkedList();
 
-        Console.WriteLine("Linked List: " );
 
-        
+            list.insertNode(56);
+            list.insertNode(30);
+            list.insertNode(70);
+            list.printNodes();
+
+
+            Console.WriteLine("Delete last node");
+            list.deleteLastNode();
+            list.printNodes();
+
+        }
     }
-}       
 
 }
+          
+
