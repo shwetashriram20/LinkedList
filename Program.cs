2f1a2f27b5a3c039a1cc0a8d6f940fba28180f03
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-11 19:02:54 +0530

    method

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace LinkedList
+{
+    class Program
+{
+    public static void Main(String[] args)
+    {
+        LinkedList list = new LinkedList();
+
+        Console.WriteLine("Current Size of the list is: " + list.getListSize());
+
+        list.insertNode(56);
+        list.insertNode(30);
+        list.insertNode(70);
+        list.printNodes();
+    }
+}       
+
+}
