14209874c5e66d4fc23f1bdfa809549c2b9b0a74
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-11 20:15:08 +0530

    Node Delete

--- Program.cs
+++ Program.cs
@@ -4,11 +4,23 @@
 {
     public static void Main(String[] args)
     {
+        LinkedList list = new LinkedList();
 
-        Console.WriteLine("Linked List: " );
+            list.insertNode(56);
+            list.insertNode(30);
+            list.insertNode(40);
+            list.insertNode(70);
+            list.printNodes();
 
-        
+
+            Console.WriteLine("the list is: " + list.getListSize());
+            Console.WriteLine("Delete node from 2nd location");
+            list.deleteNNode(2);
+            list.printNodes();
+
+        }
     }
-}       
 
 }
+          
+
