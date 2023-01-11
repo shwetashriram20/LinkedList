568377a1283e1c9e10f99685868a3b9ffb9b67d8
shwetashriram20 <120362407+shwetashriram20@users.noreply.github.com>
2023-01-11 20:20:20 +0530

    Order of data

--- Program.cs
+++ Program.cs
@@ -4,11 +4,19 @@
 {
     public static void Main(String[] args)
     {
+        LinkedList list = new LinkedList();
 
-        Console.WriteLine("Linked List: " );
+            list.insertNode(56);
+            list.insertNode(30);
+            list.insertNode(40);
+            list.insertNode(70);
+            list.printNodes();
 
-        
+            list.orderNodes();
+
+        }
     }
-}       
 
 }
+          
+
